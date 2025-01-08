b3e7a2ce9853a8892471a4a8b49a7453f88679e5
ZenithHadal <zenithhadal@gmail.com>
2025-01-08 18:36:06 -0300

    Initial commit

---
+++ src/main/java/dyingember/modid/CSMPDyingEmberClient.java
@@ -0,0 +1,10 @@
+package dyingember.modid;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CSMPDyingEmberClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
